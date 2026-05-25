c678fa21eb9cc3c2360d82c4e817350e3a4ad9a4
Gabriel Oliveira Santos <gabrielosan30042011@gmail.com>
2026-05-24 23:31:17 -0300

    exercicio_16

---
+++ exercicio_16.py
@@ -0,0 +1,6 @@
+def soma (n1,n2):
+    return n1 + n2
+numero_1 = float(input("qual o primeiro numero? "))
+numero_2 = float(input("qual o segundo numero? "))
+resultado = soma (numero_1,numero_2)
+print(resultado)
